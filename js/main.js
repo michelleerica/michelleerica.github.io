8fbfa98c8b878f084dfa98526b830899b0f663cc
Michelle Erica <michelle.erica.lo@gmail.com>
2017-08-11 19:22:13 +1000

    formatting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage();
+
+	$('.bujoco-info').click(function(){
+    $('.bujoco-modal').modal('show');
+  });
+  $(".bujoco-modal").modal({
+  closable: true
+  });
+
+	$('.fraga-info').click(function(){
+    $('.fraga-modal').modal('show');
+  });
+  $(".fraga-modal").modal({
+  closable: true
+  });
+
+	$('.figgie-info').click(function(){
+    $('.figgie-modal').modal('show');
+  });
+  $(".figgie-modal").modal({
+  closable: true
+  });
+
+	$('.tic-info').click(function(){
+    $('.tic-modal').modal('show');
+  });
+  $(".tic-modal").modal({
+  closable: true
+  });
 });
