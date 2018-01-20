c0bc2f20323e2dfe34507f59fb4cbb40f014315e
Michello Lo <michelle.erica.lo@gmail.com>
2018-01-20 18:28:05 +1100

    portfolio update with typeit

--- js/main.js
+++ js/main.js
@@ -1,68 +1,56 @@
 $(document).ready(function() {
-	// $('#fullpage').fullpage();
-	// $('#fullpage').fullpage({
-	//      scrollOverflowOptions: {
-	//           click: true
-	//      }
-	// });
-	// if (document.documentElement.clientWidth > 1023) {
-	//     $('#fullpage').fullpage({
-	//         // we need to enable scrolling on desktop version
-	//         scrollOverflow: true
-	//         // the rest setting can goes below here
-	//     });
-	// }
-	// if (document.documentElement.clientWidth < 1024) {
-	//     // $('#fullpage').fullpage({
-	//         // we need to disable scrolling not on desktop version
-	//         scrollOverflow: false
-	//         // the rest setting can goes below here
-	//     // });
-	//
-	// 		// $('#about-long').css("font-size","12px");
-	//
-	// }
 
+	$('#fullpage').fullpage({
+    sectionsColor: ['#fbd2d7', '#9ABCA7', '#AF9FA5', '#C0B9DD']
+  });
 
-	// var quoteChange = function(){
-	// 	$('#home-info').css("display","none");
-	// 	$('#funny').css("display","block");
-	//
-	// }
-	//
-	// function myFunction() {
-	//     setInterval(function(){ alert("Hello"); }, 3000);
-	// }
-	// setInterval(function(){
-	// 	$('#home-info').css("display","none");
-	// 	$('#funny').css("display","block");
-	// }, 2000);
+	new TypeIt('#typeit', {
+     speed: 50,
+     autoStart: false
+		})
+		.type('Hey')
+		.pause(500)
+		.delete(1)
+		.type('llo, ')
+		.pause(1000)
+		.type("my name is Michelle Erica Lo")
+		.break()
+		.type("(or <i>Milo</i>, for short)")
+		.break() .pause(750)
+		.type("I'm a junior web developer from Sydney")
+		.break() .pause(750)
+		.type("and teaching assistant for General Assembly's Web Development Immersive.")
+		.break() .pause(750)
+		// .options({speed: 1000})
+		.type('...')
+		.pause(1000)
+		.delete(4)
+		.break()
+		.break().pause(1000)
+		// .options({speed: 500})
+		.type('I\'m an <a href="#projects1" span class="underline--magical">avid list maker</a></span>, <a href="#projects1" span class="underline--magical">question asker</a></span> and <a href="#projects2" span class="underline--magical">intermittent foodie</a> </span>')
+		.break() .pause(1000)
+		// .options({speed: 700})
+		.type('and my projects reflect that.')
+		.pause(1000)
+		.break()
+		.type("But there's so much more to my story than that ...")
+		.break()
+		.break()
+		.pause(750)
+		.type('<i class="fa fa-hand-o-down" aria-hidden="true"></i>')
+		.break()
+		.pause(750)
+		.type('<i class="fa fa-hand-o-down" aria-hidden="true"></i>')
+		.break()
+		.pause(750)
+		.type('<i class="fa fa-hand-o-down" aria-hidden="true"></i>')
+		.break()
+		.pause(750)
+		.type('<i class="fa fa-hand-o-down" aria-hidden="true"></i>')
+		.break()
+		.pause(750)
 
-	// setInterval(function(){
-	// 	$('#funny').css("display","none");
-	// 	$('#home-info').css("display","block");
-	// }, 100
-// 	// ,
-// 	setInterval(function(){
-// 		$('#home-info').css("display","none");
-// 		$('#funny').css("display","block");
-// 	}, 1000, 	setInterval(function(){
-// 			$('#funny').css("display","none");
-// 			$('#home-info').css("display","block");
-// 		}, 1000))
-// // );
-//
-// setInterval(function(){
-// 			// Immediate
-// 			$('#funny').css("display","none");
-// 			$('#home-info').css("display","block");
-//
-// 			// In 2000 ms
-// 			setTimeout(function(){
-// 				$('#home-info').css("display","none");
-// 				$('#funny').css("display","block");
-// 			}, 2000);
-// 	}, 4000);
 
 
 
@@ -95,5 +83,5 @@ $(document).ready(function() {
   closable: true
   });
 
-	
+
 });
